2473a43420c3e8dfe52706f49fadbf635e9ae540
jaredkitade <jkitade@cca.edu>
2015-11-02 15:48:41 -0800

    k

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Map</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">    
+</head>
+
+<body>
+	<div class='map-container'>
+		<div class='map'><img src="map2.png"></div>
+		<a href='#houston'>
+			<div class='location houston'>
+				<div class="ring"></div>
+			</div>
+		</a>
+		<div class='location miramar'>
+			<div class="ring"></div>
+		</div>
+	</div>
+		<?php include 'houston.html'; ?>
+
+</body>
+</html>
